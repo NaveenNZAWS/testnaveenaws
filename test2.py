30d32a7ce4fbe0ce5acef88c9ffd3e2b97aab5fc
NaveenNZAWS <joswa12@gmail.com>
2023-10-31 22:56:26 +1300

    test

--- test2.py
+++ test2.py
@@ -1,7 +1,11 @@
 
-a=15
-b=23
-c =100
-print(a*b)
-print(a*b)
-print(a*c)
+# Input a decimal number
+decimal_number = int(input("Enter a decimal number: "))
+
+# Convert to binary and remove the '0b' prefix
+binary_representation = bin(decimal_number)[2:]
+
+print(f"The binary representation of {decimal_number} is: {binary_representation}")
+
+
+
